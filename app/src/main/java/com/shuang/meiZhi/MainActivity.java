7e801c9961b855c276aeb8194dacf73e2c4447af
shuangqingfeng <shuang199335@gmail.com>
2017-05-18 18:03:20 +0800

    '添加头像选择对话框'

--- app/src/main/java/com/shuang/meiZhi/MainActivity.java
+++ app/src/main/java/com/shuang/meiZhi/MainActivity.java
@@ -1,12 +1,21 @@
 package com.shuang.meiZhi;
 
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.RemoteException;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,6 +23,11 @@ import android.widget.FrameLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import com.hwangjr.rxbus.RxBus;
+import com.hwangjr.rxbus.annotation.Subscribe;
+import com.hwangjr.rxbus.annotation.Tag;
+import com.hwangjr.rxbus.thread.EventThread;
+import com.orhanobut.logger.Logger;
 import com.shuang.meiZhi.android.AndroidFragment;
 import com.shuang.meiZhi.android.AndroidModule;
 import com.shuang.meiZhi.android.AndroidPresenter;
@@ -29,8 +43,14 @@ import com.shuang.meiZhi.utils.StatusBarUtils;
 import com.shuang.meiZhi.utils.ToastUtils;
 import com.shuang.meiZhi.utils.UIUtils;
 import com.shuang.meiZhi.video.VideoFragment;
+import com.shuang.meiZhi.view.AlertDialogView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends BaseActivity {
@@ -38,6 +58,9 @@ public class MainActivity extends BaseActivity {
     private static final String IOS_FRAGMENT_TAG = "iosFragment";
     private static final String MEI_ZHI_FRAGMENT_TAG = "meiZhiFragment";
     private static final String VIDEO_FRAGMENT_TAG = "videoFragment";
+    private static final String SAVE_AVATORNAME = "avatar.png";// 保存的图片名
+    private static final int CAMERA_REQUEST_CODE = 1; // 拍照
+    private static final int RESULT_REQUEST_CODE = 2; // 裁剪
     @BindView(R.id.dl_drawLayout)
     DrawerLayout mDrawerLayout;
     @BindView(R.id.fl_mainContainer)
@@ -54,8 +77,8 @@ public class MainActivity extends BaseActivity {
     RadioButton itemVideo;
     @BindView(R.id.ngv_drawView)
     NavigationView navigationView;
-    @BindView(R.id.avatar_image)
-    CircleImageView avatarImg;
+
+
     private AndroidFragment androidFragment;
     private IosFragment iosFragment;
     private BeautyFragment meiZhiFragment;
@@ -64,15 +87,24 @@ public class MainActivity extends BaseActivity {
     private AndroidPresenter androidPresenter;
     private IosPresenter mIosPresenter;
     private long mExitTme = 0;
+    private CircleImageView avatarImg;
+    private ActionBarDrawerToggle mToggle;
+
 
     @Override
     protected int getContainerId() {
         return R.layout.activity_main;
     }
 
+    @SuppressWarnings("deprecation")
     @Override
     protected void initToolbar(Toolbar toolbar) {
         toolbar.setTitle(UIUtils.getString(R.string.app_name));
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        mDrawerLayout.setDrawerListener(mToggle);
+        mToggle.syncState();
+
 //        toolbar.setlog
     }
 
@@ -85,6 +117,8 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void initView(Bundle savedInstanceState) {
         navigationView.setItemIconTintList(null);
+        View navigationHeaderView = navigationView.getHeaderView(0);
+        avatarImg = (CircleImageView) navigationHeaderView.findViewById(R.id.avatar_image);
     }
 
     @Override
@@ -98,6 +132,7 @@ public class MainActivity extends BaseActivity {
             itemAndroid.setChecked(true);
         }
         switchFragmentShow(0);
+
         checkItem.setOnCheckedChangeListener(new CheckItemChangeListener());
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -142,12 +177,107 @@ public class MainActivity extends BaseActivity {
     class AvatarImgClickListener implements View.OnClickListener {
         @Override
         public void onClick(View v) {
+            if (mDrawerLayout.isDrawerOpen(navigationView)) {
+                mDrawerLayout.closeDrawers();
+            }
+            final AlertDialogView alertDialogView = new AlertDialogView(MainActivity.this, false).builder();
+            alertDialogView.show();
+            alertDialogView.setDialogChildAtViewClickListener(new AlertDialogView.DialogChildAtViewClickListener() {
+                @Override
+                public void clickCarmera() {
+                    openCarmera();
+                    alertDialogView.dismiss();
+                }
 
+                @Override
+                public void clickPhoto() {
+
+                    alertDialogView.dismiss();
+                }
+
+                @Override
+                public void clickMore() {
+                    ToastUtils.show("敬请期待");
+                    alertDialogView.dismiss();
+
+                }
+            });
+        }
+    }
+
+    private void openCarmera() {
+        Intent intentFromCapture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intentFromCapture.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File(Environment
+                .getExternalStorageDirectory(), SAVE_AVATORNAME)));
+        startActivityForResult(intentFromCapture, CAMERA_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != RESULT_CANCELED) {
+            switch (requestCode) {
+                case CAMERA_REQUEST_CODE:
+                    startPhotoZoom(Uri.fromFile(new File(Environment
+                            .getExternalStorageDirectory(), SAVE_AVATORNAME)));
+                    break;
+                case RESULT_REQUEST_CODE:
+                    if (data != null) {
+                        getImageToView(data);
+                    }
+                    break;
+            }
+        }
+    }
+
+    /**
+     * 裁剪图片方法实现
+     */
+    public void startPhotoZoom(Uri uri) {
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(uri, "image/*");
+        // 设置裁剪
+        intent.putExtra("crop", "true");
+        // aspectX aspectY 是宽高的比例
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        // outputX outputY 是裁剪图片宽高
+        intent.putExtra("outputX", 300);
+        intent.putExtra("outputY", 300);
+        intent.putExtra("return-data", true);
+        startActivityForResult(intent, RESULT_REQUEST_CODE);
+    }
+
+    /**
+     * 保存裁剪之后的图片数据
+     */
+    private void getImageToView(Intent data) {
+        Bundle extras = data.getExtras();
+        sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(new File(Environment
+                .getExternalStorageDirectory(), SAVE_AVATORNAME))));
+        if (extras != null) {
+            Bitmap photo = extras.getParcelable("data");
+            saveMyBitmap(photo); // 保存裁剪后的图片到SD
+            avatarImg.setImageBitmap(photo);
+        }
+    }
+
+    /**
+     * 将头像保存在SDcard
+     */
+    public void saveMyBitmap(Bitmap bitmap) {
+        File f = new File(Environment.getExternalStorageDirectory(), SAVE_AVATORNAME);
+        try {
+            f.createNewFile();
+            FileOutputStream fOut = new FileOutputStream(f);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+            fOut.flush();
+            fOut.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
     public void switchFragmentShow(int id) {
-
         if (null == fragmentManager) {
             fragmentManager = getSupportFragmentManager();
         }
@@ -218,25 +348,26 @@ public class MainActivity extends BaseActivity {
         }
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        MenuInflater inflater = getMenuInflater();
-//        inflater.inflate(R.menu.menu, menu);
-//        return true;
-//    }
-
     @Override
     public void onBackPressed() {
-        if (mDrawerLayout.isDrawerOpen(navigationView)) {
-            mDrawerLayout.closeDrawers();
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
 
         } else if (System.currentTimeMillis() - mExitTme > 2000) {
             ToastUtils.show("在按一次退出程序");
             mExitTme = System.currentTimeMillis();
         } else {
             this.finish();
+            android.os.Process.killProcess(android.os.Process.myPid());
             System.exit(0);
         }
-
     }
+    /**
+     * 菜单键点击的事件处理
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return mToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    }
+
 }
--- app/src/main/java/com/shuang/meiZhi/view/AlertDialogView.java
+++ app/src/main/java/com/shuang/meiZhi/view/AlertDialogView.java
@@ -3,6 +3,7 @@ package com.shuang.meiZhi.view;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
+import android.provider.SyncStateContract;
 import android.support.annotation.StyleRes;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -10,8 +11,16 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.hwangjr.rxbus.RxBus;
+import com.hwangjr.rxbus.annotation.Produce;
+import com.hwangjr.rxbus.annotation.Tag;
+import com.orhanobut.logger.Logger;
+import com.shuang.meiZhi.Constants;
 import com.shuang.meiZhi.R;
+
+import org.greenrobot.eventbus.EventBus;
 
 import butterknife.BindView;
 import retrofit2.Retrofit;
@@ -22,50 +31,62 @@ import retrofit2.Retrofit;
  * @date 2017/5/17
  */
 public class AlertDialogView implements View.OnClickListener {
+    private static final int CAREMA = 1;
+
     private Context mContext;
     private boolean mCancelable;
-    private int mThemeResId;
+    private int mThemeResId = R.style.dialogStyle;
     private Dialog mDialog;
-    private ImageView camera;
-    private ImageView photo;
-    private ImageView more;
+    private TextView camera;
+    private TextView photo;
+    private TextView more;
     private ImageView close;
+    private DialogChildAtViewClickListener mDialogChildAtViewClickListener;
 
     public AlertDialogView(Context context) {
         this(context, false);
     }
 
     public AlertDialogView(Context context, boolean cancelable) {
-        this(context, cancelable, 0);
+        this(context, cancelable, R.style.dialogStyle);
     }
 
     public AlertDialogView(Context context, boolean cancelable, @StyleRes int themeResId) {
         mContext = context;
         mCancelable = cancelable;
         mThemeResId = themeResId;
-
+        RxBus.get().register(this);
     }
 
     public AlertDialogView builder() {
         mDialog = new Dialog(mContext, mThemeResId);
         mDialog.setCanceledOnTouchOutside(mCancelable);
         View view = LayoutInflater.from(mContext).inflate(R.layout.layout_bottom_dialog, null);
-        camera = (ImageView) view.findViewById(R.id.iv_camera);
-        photo = (ImageView) view.findViewById(R.id.iv_photo);
-        more = (ImageView) view.findViewById(R.id.iv_more);
+        camera = (TextView) view.findViewById(R.id.iv_camera);
+        photo = (TextView) view.findViewById(R.id.iv_photo);
+        more = (TextView) view.findViewById(R.id.iv_more);
         close = (ImageView) view.findViewById(R.id.iv_close);
+        camera.setOnClickListener(this);
+        photo.setOnClickListener(this);
+        more.setOnClickListener(this);
+        close.setOnClickListener(this);
         mDialog.setContentView(view);
         Window window = mDialog.getWindow();
-        window.setGravity(Gravity.LEFT | Gravity.BOTTOM);
+        window.setGravity(Gravity.BOTTOM);
         WindowManager.LayoutParams lp = window.getAttributes();
         lp.x = 0;
         lp.y = 0;
+        view.measure(0, 0);
+        lp.width = (int) mContext.getResources().getDisplayMetrics().widthPixels;
+        lp.height = view.getMeasuredHeight();
+        lp.alpha = 9f; // 透明度
         window.setAttributes(lp);
         return this;
     }
 
     public void show() {
         mDialog.show();
+
 
     }
 
@@ -78,17 +99,29 @@ public class AlertDialogView implements View.OnClickListener {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.iv_camera:
-
+                mDialogChildAtViewClickListener.clickCarmera();
                 break;
             case R.id.iv_photo:
-
+                mDialogChildAtViewClickListener.clickPhoto();
                 break;
             case R.id.iv_more:
-
+                mDialogChildAtViewClickListener.clickMore();
                 break;
             case R.id.iv_close:
-
+                dismiss();
                 break;
         }
+    }
+
+    public void setDialogChildAtViewClickListener(DialogChildAtViewClickListener dialogChildAtViewClickListener) {
+        mDialogChildAtViewClickListener = dialogChildAtViewClickListener;
+    }
+
+    public interface DialogChildAtViewClickListener {
+        void clickCarmera();
+
+        void clickPhoto();
+
+        void clickMore();
     }
 }
